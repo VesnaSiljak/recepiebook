f4301d109bd0748a755d45bfcb72435d5770d5a9
Vesna <vesnaveki48@gmail.com>
2019-02-12 19:57:16 +0100

    creating components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from 'src/app/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent ,
+  children: [
+    { path: '', component: HomeComponent },
+    ]
+},
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    LoginComponent,
+    HomeComponent,
+ 
   ],
   imports: [
     BrowserModule,
